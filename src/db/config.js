1a5f5f743f56378fb48f81fc8cb871639417d2ff
Gabriel Willian <obielwb@gmail.com>
2021-07-01 12:08:56 -0300

    update Database

--- src/db/config.js
+++ src/db/config.js
@@ -1,9 +1,8 @@
 const { open } = require('sqlite')
 const sqlite3 = require('sqlite3')
 
-module.exports = () => {
+module.exports = () => 
     open({
-        filename: '/src/db/rocketq.sqlite',
-        driver: sqlite3.Database
+        filename: './src/db/rocketq.sqlite',
+        driver: sqlite3.Database,
     })
-}
